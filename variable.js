4177e93e04b665efd7402af424a7a570a1c93639
soundaryar23 <soundaryar58@gmail.com>
2024-06-20 23:19:45 +0530

    new fies

---
+++ variable.js
@@ -0,0 +1,22 @@
+var names;
+names ="shriyan";
+console.log(names,typeof(names));
+names=120
+console.log(names, typeof(names));
+var b="26";
+console.log(typeof b);
+var na ="shriyan";
+console.log(typeof na);
+
+let contactNo= 2244782255;
+console.log(contactNo)
+ contactNo=100;
+console.log(contactNo)
+contactNo=100;
+console.log(contactNo)
+
+const numbers=2355787;
+console.log(numbers)
+
+ numbers=2355787;
+console.log(numbers)
